de5d88c440be3595889a885693bf0e866254876a
b1ackmai1er <39449559+b1ackmai1er@users.noreply.github.com>
2018-11-27 20:47:25 +0800

    Allow command line argument for device to test

--- piociotst.asm
+++ piociotst.asm
@@ -1,9 +1,13 @@
 ;
 ; CP/M Z80 HBIOS CIO PIO BIT MODE TEST PROGRAM
 ;
-; Version 20-Nov-2018 
+; Version 27-Nov-2018 
 ; Phil Summers
 ; difficultylevelhigh@gmail.com
+; Some code portions by Wayne Warthen. Thank you.
+;
+; USAGE: PIOCIOTST n
+;
 ;
 ; Default test pattern for each port is:
 ; 
@@ -12,20 +16,35 @@
 ; 3 Toggle alternate bits on and off. 
 ;
 ;
-UNIT	.EQU	$07
+RESTART	.EQU	$0000
 BDOS	.EQU	0005H
 BDOUT	.EQU	09H	
 CR	.EQU	0DH
 LF	.EQU	0AH
+stksiz	.equ	$40		; Working stack size
 
 	.ORG	100H
 ;
-	LD	DE,MSGBEG	; HELLO
+	ld	(stksav),sp	; save stack
+	ld	sp,stack	; set new stack
+
+RUN:	LD	DE,MSGBEG	; HELLO
 	CALL	PSTRIN
 
+	ld	ix,$81		; point to start of parm area (past len byte)
+SKIP:	ld	a,(ix)		; load next character
+	or	a		; string ends with a null
+	jr	z,GETDEV	; if null, return pointing to null
+	cp	' '		; check for blank
+	JR	nz,GETDEV	; return if not blank
+	inc	ix		; if blank, increment character pointer
+	jr	SKIP		; and loop
+
+GETDEV:	CALL	getnum		; DEVICE TO TEST IS IN A & C
+	LD	(UNIT),A
 	LD	B,$04		; INIT 
-	LD	C,UNIT
 	LD	DE,$FFFF
+	RST	08
 
 	CALL	PAUSE
 	CALL	PAUSE
@@ -44,9 +63,9 @@ LP2:	LD	A,(HL)
 	LD	E,A
 ;
 	PUSH	BC
-;	LD	E,$00
 	LD	B,$01
-	LD	C,UNIT
+	LD	A,(UNIT)
+	LD	C,A
 	RST	08
 	POP	BC
 	CALL	PAUSE
@@ -57,7 +76,9 @@ LP2:	LD	A,(HL)
 ;
 BYE:	LD	DE,MSGEND	; GOODBYE
 	CALL	PSTRIN	
-	RET
+
+	ld	sp,(stksav)	; restore stack
+	JP	RESTART		; return to CP/M via restart
 ;
 ; PAUSE
 ;
@@ -138,6 +159,46 @@ BDO:	PUSH	HL
 	POP	BC
 	RET
 
+;
+; Get numeric chars and convert to number returned in A
+; Carry flag set on overflow
+;
+getnum:
+	ld	c,0		; C is working register
+getnum1:
+	ld	a,(ix)		; get the active char
+	cp	'0'		; compare to ascii '0'
+	jr	c,getnum2	; abort if below
+	cp	'9' + 1		; compare to ascii '9'
+	jr	nc,getnum2	; abort if above
+;
+	; valid digit, add new digit to C
+	ld	a,c		; get working value to A
+	rlca			; multiply by 10
+	ret	c		; overflow, return with carry set
+	rlca			; ...
+	ret	c		; overflow, return with carry set
+	add	a,c		; ...
+	ret	c		; overflow, return with carry set
+	rlca			; ...
+	ret	c		; overflow, return with carry set
+	ld	c,a		; back to C
+	ld	a,(ix)		; get new digit
+	sub	'0'		; make binary
+	add	a,c		; add in working value
+	ret	c		; overflow, return with carry set
+	ld	c,a		; back to C
+;
+	inc	ix		; bump to next char
+	jr	getnum1		; loop
+;
+getnum2:	; return result
+	ld	a,c		; return result in A
+	or	a		; with flags set, CF is cleared
+	ret
+
+UNIT:	.DB	0
+
 MSGBEG:	.DB	"CP/M Z80 HBIOS PIO BIT MODE TEST.",CR,LF,"$"
 MSGEND:	.DB	CR,LF,"TEST COMPLETE.",CR,LF,"$"
 PIONUM:	.DB	"PIO $"
@@ -170,5 +231,9 @@ TSTBEG	.EQU	$
 	.DB	0FFH
 	.DB	00H
 TSTPAT:	.DB	0FFH	
+
+stksav	.dw	0		; stack pointer saved at start
+	.fill	stksiz,0	; stack
+stack	.equ	$
 
         .END
